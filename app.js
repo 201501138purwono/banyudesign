d1b7c921df0e9099379fce1db4d883a96fc3cfe5
Ipung Purwono 201501138 <ipungofficial@gmail.com>
2018-04-26 00:05:34 +0700

    ubah app.js

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ var express = require('express');
 var app = express();
 var port = 3000;
 app.listen(port, function(){
-  console.log('hello world');
+  console.log('Server is running on port:', port);
 })
+app.get('/', function(req, res){
+    res.send('Hello Express');
+});
